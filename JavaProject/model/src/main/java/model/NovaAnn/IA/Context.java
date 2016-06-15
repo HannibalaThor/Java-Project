4b8000bc26854110bc13e49cdae53c8cdd1fdc0c
clementerb <clementerb@25.60.131.21>
2016-06-15 13:33:33 +0200

    package AI with void tests

---
+++ JavaProject/model/src/main/java/model/NovaAnn/IA/Context.java
@@ -0,0 +1,15 @@
+package model.NovaAnn.IA;
+
+
+public class Context {
+	private IStrategy strategy;
+	
+	public Context(IStrategy strategy){
+		
+	}
+	
+	public void executeStrategy(){
+		
+	}
+
+}
